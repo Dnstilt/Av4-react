83313c8dadc451f6c607210c4535809680f5ac62
alvaro_a_oliveira <alvaro.a.oliveira36@gmail.com>
2024-06-21 19:35:56 -0300

    Commit inicial

---
+++ hora_decidida/src/components/politicaDePrivacidade/Politica.jsx
@@ -0,0 +1,19 @@
+import "../../App.css"
+
+const politicaDePrivacidade = () => {
+    return(
+        <main className="d-flex flex-wrap justify-content-center">
+            <div className="flex-row p-5" id="containerInterno" >
+            <p className="fs-1">Política de privacidade</p>
+            <p>
+                Ao utilizar nosso sistema, você entende que estaremos coletando e usaremos algumas de suas informações pessoais na formas descritas nessa política, sob as normas de proteção de dados(LGPD, Lei Federal 13.709/2018), das disposições consumeristas da Lei Federal 8078/1990 e as demais normas do ordenamento jurídico brasileiro aplicáveis.
+                Dessa forma, a Hora decidida, inscrita no CNPJ/MF sob o nº xx.xxx.xxx/xxx1-xx, no papel de Controladora de Dados, se obriga ao disposto na presente Política de Privacidade.
+            </p>
+            <p className="fs-3">1. O que são dados pessoais?</p>
+            <p>
+                Dados pessoais são informações relativas a uma pessoa natural identificada ou identificável. Por exemplo, nome, endereço, carteira de identidade, nome de usuário e e-mail são dados pessoais em potencial.
+                Dados pessoais sensíveis são aqueles referentes a origem racial ou étnica, convicção religiosa, opinião política, filiação a sindicato ou a organização de caráter religioso, filosófico ou político, dado referente à saúde ou à vida sexual, dado genético ou biométrico do titular.
+                Note que nem todos os dados são necessariamente dados pessoais. Os dados tratados pela Hora decidida podem não revelar a identidade específica da pessoa natural, como por exemplo quando coletamos informações sobre o navegador, sistema operacional, plataforma e/ou modelo de aparelho celular utilizado.
+            </p>
+
+
