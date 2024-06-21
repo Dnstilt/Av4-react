ac8469e21090e486cfec42d18fb76c3b89727fdc
alvaro_a_oliveira <alvaro.a.oliveira36@gmail.com>
2024-06-21 19:56:06 -0300

    Commit 2

--- hora_decidida/src/components/politicaDePrivacidade/Politica.jsx
+++ hora_decidida/src/components/politicaDePrivacidade/Politica.jsx
@@ -1,19 +1,68 @@
 import "../../App.css"
 
 const politicaDePrivacidade = () => {
-    return(
+    return (
         <main className="d-flex flex-wrap justify-content-center">
-            <div className="flex-row p-5" id="containerInterno" >
-            <p className="fs-1">Política de privacidade</p>
-            <p>
-                Ao utilizar nosso sistema, você entende que estaremos coletando e usaremos algumas de suas informações pessoais na formas descritas nessa política, sob as normas de proteção de dados(LGPD, Lei Federal 13.709/2018), das disposições consumeristas da Lei Federal 8078/1990 e as demais normas do ordenamento jurídico brasileiro aplicáveis.
-                Dessa forma, a Hora decidida, inscrita no CNPJ/MF sob o nº xx.xxx.xxx/xxx1-xx, no papel de Controladora de Dados, se obriga ao disposto na presente Política de Privacidade.
-            </p>
-            <p className="fs-3">1. O que são dados pessoais?</p>
-            <p>
-                Dados pessoais são informações relativas a uma pessoa natural identificada ou identificável. Por exemplo, nome, endereço, carteira de identidade, nome de usuário e e-mail são dados pessoais em potencial.
-                Dados pessoais sensíveis são aqueles referentes a origem racial ou étnica, convicção religiosa, opinião política, filiação a sindicato ou a organização de caráter religioso, filosófico ou político, dado referente à saúde ou à vida sexual, dado genético ou biométrico do titular.
-                Note que nem todos os dados são necessariamente dados pessoais. Os dados tratados pela Hora decidida podem não revelar a identidade específica da pessoa natural, como por exemplo quando coletamos informações sobre o navegador, sistema operacional, plataforma e/ou modelo de aparelho celular utilizado.
-            </p>
-
-
+            <div className="flex-row p-5" id="containerInterno">
+                <p className="fs-1">Política de privacidade</p>
+                <p>
+                    Ao utilizar nosso sistema, você entende que estaremos coletando e usaremos algumas de suas
+                    informações pessoais na formas descritas nessa política, sob as normas de proteção de dados(LGPD,
+                    Lei Federal 13.709/2018), das disposições consumeristas da Lei Federal 8078/1990 e as demais normas
+                    do ordenamento jurídico brasileiro aplicáveis.
+                    Dessa forma, a Hora decidida, inscrita no CNPJ/MF sob o nº xx.xxx.xxx/xxx1-xx, no papel de
+                    Controladora de Dados, se obriga ao disposto na presente Política de Privacidade.
+                </p>
+                <p className="fs-3">1. O que são dados pessoais?</p>
+                <p>
+                    Dados pessoais são informações relativas a uma pessoa natural identificada ou identificável. Por
+                    exemplo, nome, endereço, carteira de identidade, nome de usuário e e-mail são dados pessoais em
+                    potencial.
+                    Dados pessoais sensíveis são aqueles referentes a origem racial ou étnica, convicção religiosa,
+                    opinião política, filiação a sindicato ou a organização de caráter religioso, filosófico ou
+                    político, dado referente à saúde ou à vida sexual, dado genético ou biométrico do titular.
+                    Note que nem todos os dados são necessariamente dados pessoais. Os dados tratados pela Hora decidida
+                    podem não revelar a identidade específica da pessoa natural, como por exemplo quando coletamos
+                    informações sobre o navegador, sistema operacional, plataforma e/ou modelo de aparelho celular
+                    utilizado.
+                </p>
+                <p className="fs-3">2. Quais dados coletamos sobre você e para qual finalidade?</p>
+                <p>Coletamos, tratamos, armazenamos e utilizamos alguns dados pessoais, para o cumprimento de obrigação
+                    legal ou regulatória; quando necessário para a execução de contrato ou de procedimentos preliminares
+                    relacionados a contrato do qual seja parte o titular, a pedido do titular dos dados; para o
+                    exercício regular de direitos em processo judicial, administrativo ou arbitral, e, quando necessário
+                    para atender aos interesses legítimos do controlador ou de terceiros.</p>
+                <p>Por exemplo, podemos coletar seus dados pessoais para os seguintes fins:</p>
+                <p>
+                    (a) Para customizar sua experiência e navegação, exibindo seu nome, e-mail, endereço de IP e data e
+                    local de seu acesso. Estas informações são utilizadas somente para fins de segurança, limitação e
+                    validação de tráfego dentro das nossas plataformas.
+                </p>
+                <p>
+                    (b) Para entrar em contato com você, caso você realize cadastro em nossa plataforma digital, ou caso
+                    seja necessário contatá-lo acerca de algum assunto importante na plataforma. Os dados pessoais
+                    tratados são dados de identificação, dados para contato e demais dados transmitidos por você à Hora
+                    Decidida. Trataremos seus dados com base em seu consentimento ou em nosso legítimo interesse, a
+                    depender do caso.
+                </p>
+                <p>
+                    (c) Para responder uma solicitação sua através de nosso canal de suporte, hipótese em que trataremos
+                    os dados transferidos por você à Hora Decidida, dados de identificação e dados de contato. Estes
+                    dados serão tratados para fins de atendimento a obrigações regulatórias ou legais, com base em nosso
+                    legítimo interesse em melhor atender às suas solicitações.
+                </p>
+                <p>
+                    (d) Para promoção de nossos produtos e atividades por meio de promoções internas ou de parceiros,
+                    hipótese em que trataremos seus dados de históricos, informações coletadas através de formulários,
+                    cookies, ferramentas de analytics, dados de contato e cadastro no aplicativo da Hora Decidida. Os
+                    dados serão tratados com base em nosso legítimo interesse em promover nossas atividades ou com base
+                    em seu consentimento, por exemplo, quando você assina nossa Newsletter.
+                </p>
+                <p>
+                    (e) Para melhorar sua experiência em nosso aplicativo e permitir seu funcionamento de maneira
+                    eficaz, bem como atualização de registros internos.
+                </p>
+                <p>
+                    (f) Para defender nossos interesses em eventual processo judicial, administrativo ou arbitral. Neste
+                    caso, poderemos utilizar todos os dados pessoais relativos à lide em questão.
+                </p>
