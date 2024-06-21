40cb1fb78845c905b03169ea49464840d63b09eb
Dnstilt <denistakenaka@gmail.com>
2024-06-21 15:03:27 -0300

    Arrengin routs

---
+++ AppCadastro/src/pages/ParceirosPage.jsx
@@ -0,0 +1,12 @@
+
+import Parceiros from '../componentes/Parceiros/Parceiros'
+
+const ParceirosPage = () => {
+  return (
+    <>
+     <Parceiros/>
+    </>
+  );
+};
+
+export default ParceirosPage;
---
+++ AppCadastro/src/pages/ProdutosPage.jsx
@@ -0,0 +1,12 @@
+import Produtos from '../componentes/Produtos/Produtos'
+
+
+const ProdutosPage = () => {
+  return (
+    <>
+     <Produtos/>
+    </>
+  );
+};
+
+export default ProdutosPage;
