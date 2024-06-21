1d3eeae1587f5ab132a72d5de025992db9f0d3ab
Dnstilt <denistakenaka@gmail.com>
2024-06-21 10:17:27 -0300

    rout commit

--- AppCadastro/src/App.jsx
+++ AppCadastro/src/App.jsx
@@ -1,16 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {
+  Route, 
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from './pages/HomePage'
+import ProductsPage from './pages/ProductsPage'
+import PartnersPage from './pages/PartnersPage'
+import PoliticaPage from './pages/PoliticaPage'
+import TermosPage from './pages/TermosPage'
+import UsersPage from './pages/UsersPage'
+import NotFoundPage from './pages/NotFoundPage'
+import MainLayout from "./Layouts/MainLayout";
+import EmpresasPage from "./pages/Empresas";
+import FuncionariosPage from "./pages/Funcionarios";
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      
-    </>
-  )
-}
-
-export default App
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />} >
+      <Route index element={ <HomePage /> }/>
+      <Route path="/produtos" element={ <ProductsPage/> }/>
+      <Route path="/parceiros" element={ <PartnersPage/> }/>
+      <Route path="/politica" element={ <PoliticaPage/> }/>
+      <Route path="/termos" element={ <TermosPage/> }/>
+      <Route path="/users" element={ <UsersPage /> }/>
+      <Route path="/Empresas" element={ <EmpresasPage /> }/>
+      <Route path="/Funcionarios" element={ <FuncionariosPage /> }/>
+      <Route path="*" element={ <NotFoundPage/> }/> 
+    </Route>  
+)
+);
+const App = () => {
+ return <RouterProvider router ={router}/>
+};
+export default App;
