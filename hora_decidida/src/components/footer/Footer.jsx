05a3a88ceba4df394c199bcb17f5b117e200dbc0
alvarooliveira09 <126297198+alvarooliveira09@users.noreply.github.com>
2024-07-10 16:25:41 -0300

    Update Footer.jsx

--- hora_decidida/src/components/footer/Footer.jsx
+++ hora_decidida/src/components/footer/Footer.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom"
 
 const Footer = () => {
     return(   
-    <div className="container rounded-1" id="footer">
+    <div className="fixed-bottom container rounded-1" id="footer">
         <div className="row p-3">
         <div className="col-sm-12 col-md-6 text-light">
-            <p className="text-dark">2024 Urutau.inc - Todos os direitos reservados</p>
+            <p className="text-dark">2024 HoraDecidida.inc - Todos os direitos reservados</p>
               
-            <p className="text-dark">Rua Laurindo José de Souza, 1150 Barra da lagoa - florianópolis SC</p>
+            <p className="text-dark">Rua Laurindo José de Souza, 1150 Barra da lagoa - Florianópolis SC</p>
         </div>
         <div className="col-sm-12 col-md-6 text-end d-flex flex-column">
             <Link to="/politica" className="text-decoration-none text-dark">Política de privacidade</Link>
